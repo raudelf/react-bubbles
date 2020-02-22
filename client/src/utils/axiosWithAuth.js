999069bc6d362ca28c35dfd1eceabf040ffb224e
Raudel Flores <rawdell@outlook.com>
2020-02-21 20:27:01 -0600

    fixed code, I was missing 'return' in my function

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
 
-    axios.create({
+    return axios.create({
         headers: {
             Authorization: token
         },
